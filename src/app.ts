72df775ceb4a70c6f5b750e5a5b89d34cd241479
Dipto0000 <dipto2041@gmail.com>
2025-06-24 12:50:42 +0600

    routes issue fixed

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from "express";
+import cors from 'cors';
 
+import bookRoutes from './routes/book.route';
+import borrowRoutes from './routes/borrow.routes';
 
 const app: Application = express();
-
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to library management system");
 })
 
+
+app.use('/api/books', bookRoutes);
+app.use('/api/borrow', borrowRoutes);
 
 
 export default app;
